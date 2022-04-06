5bb82e391a644d95a4f5523d1dc773ea111722f6
RenzoGamero <renzogamero@gmail.com>
2022-04-06 08:41:38 -0500

    multiple change ts

--- src/dash_app/cross_filtering_app.ts
+++ src/dash_app/cross_filtering_app.ts
@@ -6,36 +6,9 @@ import pandas as pd
 
 
 import micropip
-micropip.install('google-drive-api')
+micropip.install('datasheets')
 
-from google_drive import DriveAPI
-import pygsheets
-gc = pygsheets.authorize(service_file='client_secrets.json')
-sh = gc.open_by_key('1tuKjiWVKWb4S_kA8aJYy3Bg1eOQDTHKb5-ZkkBSSCbI')
-worksheet1 = sh.worksheet('title', 'Hoja 1')
-sheetData = worksheet1.get_all_records()
-DFMetadata = pd.DataFrame(sheetData)
-print(DFMetadata)
-
-app = Dash(__name__)
-
-app.layout = html.Div([
-    dcc.Dropdown(['New York City', 'Montal', 'San Francisco'], 'Montal', multi=True),
-    dcc.Textarea(
-        id='textarea-example',
-        value="Textarea content initialized with multiple lines of text",
-        style={'width': '100%', 'height': 100},
-    ),
-    html.Div(id='textarea-example-output', style={'whiteSpace': 'pre-line'})
-])
-
-@app.callback(
-    Output('textarea-example-output', 'children'),
-    Input('textarea-example', 'value')
-)
-def update_output(value):
-    worksheet1.append_table(values=[value])
-    return 'You have entered: {}'.format(value)
+import datasheets
 
 
 
