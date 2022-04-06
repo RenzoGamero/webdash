172ee2166d77139f24c803e340dc434f6efd764d
RenzoGamero <renzogamero@gmail.com>
2022-04-05 22:50:42 -0500

    multiple change ts

--- src/dash_app/testinterno000.ts
+++ src/dash_app/testinterno000.ts
@@ -1,12 +1,37 @@
 //@ts-ignore
 window.dashApp = `
 from dash import Dash, dcc, html
+from dash.dependencies import Input, Output
+import pandas as pd
+import pygsheets
+gc = pygsheets.authorize(service_file='client_secrets.json')
+sh = gc.open_by_key('1tuKjiWVKWb4S_kA8aJYy3Bg1eOQDTHKb5-ZkkBSSCbI')
+worksheet1 = sh.worksheet('title', 'Hoja 1')
+sheetData = worksheet1.get_all_records()
+DFMetadata = pd.DataFrame(sheetData)
+print(DFMetadata)
 
 app = Dash(__name__)
 
 app.layout = html.Div([
-    dcc.Dropdown(['New York City', 'Montal', 'San Francisco'], 'Montal', multi=True)
+    dcc.Dropdown(['New York City', 'Montal', 'San Francisco'], 'Montal', multi=True),
+    dcc.Textarea(
+        id='textarea-example',
+        value='Textarea content initialized\nwith multiple lines of text',
+        style={'width': '100%', 'height': 100},
+    ),
+    html.Div(id='textarea-example-output', style={'whiteSpace': 'pre-line'})
 ])
+
+@app.callback(
+    Output('textarea-example-output', 'children'),
+    Input('textarea-example', 'value')
+)
+def update_output(value):
+    worksheet1.append_table(values=[value])
+
+    return 'You have entered: \n{}'.format(value)
+
 
 #if __name__ == '__main__':
 #    app.run_server(debug=True)
