a8d56cf6a94179572b65e89b8855474494c4851e
RenzoGamero <renzogamero@gmail.com>
2022-04-06 00:09:56 -0500

    multiple change ts

--- src/dash_app/cross_filtering_app.ts
+++ src/dash_app/cross_filtering_app.ts
@@ -6,9 +6,9 @@ import pandas as pd
 
 
 import micropip
-micropip.install('gspread')
+micropip.install('sheetfu')
 
-import gspread
+import sheetfu
 
 import pygsheets
 gc = pygsheets.authorize(service_file='client_secrets.json')
